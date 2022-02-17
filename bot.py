e18389306aa56ac5d842598a65a3e4d3277dbdd9
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-02-17 22:03:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from random import choice
 from pyrogram import Client, filters
 from youtube_search import YoutubeSearch
 from youtubesearchpython import VideosSearch
-from yt-dlp import YoutubeDL
+from yt_dlp import YoutubeDL
 from database import Database
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
