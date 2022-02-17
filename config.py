f3fa3c6de10d0bb6614db1ec3ba21f7efc8414d6
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-02-17 21:01:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    DATABASE = os.environ.get("DB_URI")
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
+    DATABASE = os.environ.get("DB_URI", "mongodb+srv://poojabot:poojabot@cluster0.abcos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5297426893").split())
     SUPPORT = os.environ.get("SUPPORT")
-    BOT_NAME = os.environ.get("bat")
-    BOT_USERNAME = os.environ.get("bn")
+    BOT_NAME = os.environ.get("bat","Song Download")
+    BOT_USERNAME = os.environ.get("bn",songyt_dl_bot)
